78c0d2d96d629c640a7031e0b90f088dddc1f0c8
V <hell_nam@hotmail.com>
2017-10-04 14:23:54 +0200

    Update

--- src/TestLogicResolverAPI.java
+++ src/TestLogicResolverAPI.java
@@ -6,7 +6,7 @@ public class TestLogicResolverAPI {
     public static void main(String args[]) throws Exception
     {
         boolean temp;
-        ArrayList<Clause> moreClauses = Clause.generateMoreClauses();
+        ArrayList<Clause> moreClauses = generateMoreClauses();
 
         Engine logicEngine;
         logicEngine = new Engine(moreClauses);
@@ -19,5 +19,117 @@ public class TestLogicResolverAPI {
         temp = logicEngine.getTheTruth();
 
         System.out.print("Proof by contradiction says that the statement is: " + temp + ". \n\n\n");
+    }
+
+    public static ArrayList<Clause> generateClauses(){
+        ArrayList<Clause> clauses = new ArrayList<Clause>();
+
+        Literal a = new Literal('A', true);
+        Literal b = new Literal('B', false);
+        Literal c = new Literal('C', true);
+
+        Clause clause1 = new Clause(a);
+        clause1.addLiteral(b);
+        clause1.addLiteral(c);
+
+        Literal a2 = new Literal('A', true);
+        Literal c2 = new Literal('C', false);
+
+        Clause clause2 = new Clause(a2);
+        clause2.addLiteral(c2);
+
+        clauses.add(clause1);
+        clauses.add(clause2);
+        return clauses;
+    }
+
+    public static ArrayList<Clause> generateMoreClauses(){
+/*
+        ArrayList<Clause> moreClauses = new ArrayList<Clause>();
+
+        Literal a = new Literal('A', true);
+        Literal b = new Literal('B', true);
+        Literal c = new Literal('C', true);
+
+        Clause clause1 = new Clause(a);
+        clause1.addLiteral(b);
+        clause1.addLiteral(c);
+
+        Literal a2 = new Literal('A', false);
+        Literal c2 = new Literal('C', true);
+
+        Clause clause2 = new Clause(a2);
+        clause2.addLiteral(c2);
+
+        Literal a3 = new Literal('A', true);
+        Literal c3 = new Literal('C', false);
+        Literal d = new Literal('D', true);
+
+        Clause clause3 = new Clause(a3);
+        clause3.addLiteral(c3);
+        clause3.addLiteral(d);
+
+        Literal b4 = new Literal('B', false);
+        Literal c4 = new Literal('C', false);
+
+        Clause clause4 = new Clause(b4);
+        clause4.addLiteral(c4);
+
+        moreClauses.add(clause1);
+        moreClauses.add(clause2);
+        moreClauses.add(clause3);
+        moreClauses.add(clause4);
+
+        return moreClauses;*/
+
+        ArrayList<Clause> moreClauses = new ArrayList<Clause>();
+
+
+        Literal a = new Literal('A' , true);
+        Literal a1 = new Literal('A', false);
+        Literal b = new Literal('B', true);
+        Literal b1 = new Literal('B', false);
+        Literal c = new Literal ('C', true);
+        Literal c1 = new Literal('C', false);
+        //Literal e = new Literal(' ', false);
+
+        Clause clas1 = new Clause(a1);
+        clas1.addLiteral(b);
+        moreClauses.add(clas1);
+
+        Clause clas2 = new Clause(b1);
+        clas2.addLiteral(c);
+        clas2.addLiteral(a);
+        moreClauses.add(clas2);
+
+        Clause clas3 = new Clause(c1);
+        clas3.addLiteral(b);
+        moreClauses.add(clas3);
+
+        Clause clas4 = new Clause(b1);
+        moreClauses.add(clas4);
+
+        return moreClauses;
+    }
+
+    public ArrayList<Clause> robberClauses (){
+        ArrayList<Clause> robberInfo = new ArrayList<Clause>();
+
+        Literal rA = new Literal('A', true);
+        Literal notRA = new Literal('A', false);
+        Literal rB = new Literal('B', true);
+        Literal notRB = new Literal('B', false);
+        Literal rC = new Literal('C', true);
+        Literal notRC = new Literal('C', false);
+
+        Literal dA = new Literal('X', true);
+        Literal notDA = new Literal('X', false);
+        Literal dB = new Literal('Y', true);
+        Literal notDB = new Literal('Y', false);
+        Literal dC = new Literal('Z', true);
+        Literal notDC = new Literal('Z', false);
+
+        Clause clause1 = new Clause(rA);
+        return robberInfo;
     }
 }
--- src/engine/Clause.java
+++ src/engine/Clause.java
@@ -35,7 +35,7 @@ public class Clause {
 		return this.clause;
 	}
 
-	public void updateClause(){
+	protected void updateClause(){
 
         if( !literals.isEmpty() ) {
 
@@ -60,11 +60,11 @@ public class Clause {
 
     /* TODO: fix function names once old ones are pruned
     * */
-	public Clause resolve2(Clause B){
+	protected Clause resolve2(Clause B){
 	    Clause resolvent = new Clause(this.literals);
         resolvent.addLiterals(B.literals); // Clause is now like (A|-B|C|A|-C)
         resolvent.negation2();             // Clause will be updated to (A|-B|A) TODO: remove '2'
-        resolvent.factor2();               // Clause will be updated to (A|-B). Done. TODO: remove '2'
+        resolvent.factor();               // Clause will be updated to (A|-B). Done. TODO: remove '2'
         // TODO: add internal sort
 	    return resolvent;
     }
@@ -72,8 +72,6 @@ public class Clause {
     public void sortLiteral(){
 
     }
-
-
 
     // 0 1 2 3 4 5
     // 0 1 2 3 4 5
@@ -88,7 +86,7 @@ public class Clause {
 	 * factor will find this redundant and will change the clause to (A)
 	 * Output: Factored and negated clauses
 	 * */
-	public void factor2(){
+	private void factor(){
 
 	    for(int i = 0; i < literals.size(); ++i){
             for(int j = literals.size()-1; j > i; --j){
@@ -104,7 +102,7 @@ public class Clause {
 
 	}
 
-	public boolean factorCheckMatch(int i, int j){
+	private boolean factorCheckMatch(int i, int j){
 	    return this.literals.get(i).getSymbol() == this.literals.get(j).getSymbol() &&
                 this.literals.get(i).isPositive() == this.literals.get(j).isPositive();
     }
@@ -114,13 +112,13 @@ public class Clause {
 	*
 	* */
 
-	public Boolean negationCheckMatch2(int i, int j){
+	private Boolean negationCheckMatch2(int i, int j){
 
         return  literals.get(i).getSymbol() == literals.get(j).getSymbol() &&
                 literals.get(i).isPositive() != literals.get(j).isPositive();
     }
 //  A -B C A -C
-	public void negation2(){ // is to replace current function 'negation'
+	private void negation2(){ // is to replace current function 'negation'
 
         for(int i = 0; i < literals.size();++i){
 		    for(int j = literals.size()-1; j > i; --j){
@@ -134,12 +132,6 @@ public class Clause {
             }
         }
 	}
-
-	/*  used in factor and negation respectively
-	*
-	* */
-
-
 	@Override
     public boolean equals(Object o) {
         if (o != null) {
@@ -154,112 +146,17 @@ public class Clause {
         return Objects.hash(clause);
     }
 
-	public void deleteLiteral(int j){
+	private void deleteLiteral(int j){
 	    literals.remove(j);
 	    updateClause();
     }
 
-	public void deleteLiteralPair(int i, int j){
+	private void deleteLiteralPair(int i, int j){
 
 	    //System.out.println("deletePair: size " + this.literals.size());
 	    this.literals.remove(j);
 	    this.literals.remove(i);
         //System.out.println("deletePair: size after delete " + this.literals.size());
         updateClause();
-    }
-    /** TODO: rewrite slightly to work with literals
-     *
-     *
-     */
-
-    public static ArrayList<Clause> generateClauses(){
-        ArrayList<Clause> clauses = new ArrayList<Clause>();
-
-        Literal a = new Literal('A', true);
-        Literal b = new Literal('B', false);
-        Literal c = new Literal('C', true);
-
-        Clause clause1 = new Clause(a);
-        clause1.addLiteral(b);
-        clause1.addLiteral(c);
-
-        Literal a2 = new Literal('A', true);
-        Literal c2 = new Literal('C', false);
-
-        Clause clause2 = new Clause(a2);
-        clause2.addLiteral(c2);
-
-        clauses.add(clause1);
-        clauses.add(clause2);
-        return clauses;
-    }
-
-    public static ArrayList<Clause> generateMoreClauses(){
-/*
-        ArrayList<Clause> moreClauses = new ArrayList<Clause>();
-
-        Literal a = new Literal('A', true);
-        Literal b = new Literal('B', true);
-        Literal c = new Literal('C', true);
-
-        Clause clause1 = new Clause(a);
-        clause1.addLiteral(b);
-        clause1.addLiteral(c);
-
-        Literal a2 = new Literal('A', false);
-        Literal c2 = new Literal('C', true);
-
-        Clause clause2 = new Clause(a2);
-        clause2.addLiteral(c2);
-
-        Literal a3 = new Literal('A', true);
-        Literal c3 = new Literal('C', false);
-        Literal d = new Literal('D', true);
-
-        Clause clause3 = new Clause(a3);
-        clause3.addLiteral(c3);
-        clause3.addLiteral(d);
-
-        Literal b4 = new Literal('B', false);
-        Literal c4 = new Literal('C', false);
-
-        Clause clause4 = new Clause(b4);
-        clause4.addLiteral(c4);
-
-        moreClauses.add(clause1);
-        moreClauses.add(clause2);
-        moreClauses.add(clause3);
-        moreClauses.add(clause4);
-
-        return moreClauses;*/
-
-        ArrayList<Clause> moreClauses = new ArrayList<Clause>();
-
-
-        Literal a = new Literal('A' , true);
-        Literal a1 = new Literal('A', false);
-        Literal b = new Literal('B', true);
-        Literal b1 = new Literal('B', false);
-        Literal c = new Literal ('C', true);
-        Literal c1 = new Literal('C', false);
-        //Literal e = new Literal(' ', false);
-
-        Clause clas1 = new Clause(a1);
-        clas1.addLiteral(b);
-        moreClauses.add(clas1);
-
-        Clause clas2 = new Clause(b1);
-        clas2.addLiteral(c);
-        clas2.addLiteral(a);
-        moreClauses.add(clas2);
-
-        Clause clas3 = new Clause(c1);
-        clas3.addLiteral(b);
-        moreClauses.add(clas3);
-
-        Clause clas4 = new Clause(b1);
-        moreClauses.add(clas4);
-
-        return moreClauses;
     }
 }
